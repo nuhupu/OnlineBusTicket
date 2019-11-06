fb9d25da818f375ab1b52529fa3688562575d021
nuhupu <sirachilles83@gmail.com>
2019-11-06 14:22:39 +0700

    Phuc folder c#

---
+++ OnlineBusTicket/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBusTicket.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Route()
+        {
+            return View();
+        }
+
+        public ActionResult EditRoute()
+        {
+            return View();
+        }
+
+        public ActionResult BlockTime()
+        {
+            return View();
+        }
+
+        public ActionResult AddBlockTime()
+        {
+            return View();
+        }
+
+        public ActionResult EditBlockTime()
+        {
+            return View();
+        }
+
+        public ActionResult GroupBlockTime()
+        {
+            return View();
+        }
+
+        public ActionResult AddGroupBlockTime()
+        {
+            return View();
+        }
+    }
+}
