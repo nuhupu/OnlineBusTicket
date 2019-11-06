147c094fbbf36fdeef3738245cdaaeb517a0a306
nuhupu <sirachilles83@gmail.com>
2019-11-06 18:07:43 +0700

    Phuc
    client

---
+++ OnlineBusTicket/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBusTicket.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
